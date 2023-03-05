c298882bbd1fc491f307db890626ef53de834ad0
Ella-May <20191267@cityofmalabonuniversity.edu.ph>
2023-03-06 05:02:31 +0800

    dashboard link

--- views/dashboard.php
+++ views/dashboard.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dasboard</title>
+    <link rel="stylesheet" href="stylesheets/dashboard.css">
 </head>
 <body>
    
@@ -25,8 +26,13 @@
     <main>
         <p>TRY LANG PO KUNG SAN TO MAPUPUNTA</p>
     </main>
-    <aside>
+    <aside id="Modules">
         <p>ITO RIN SAN KA PUPUNTA</p>
+        <ul>
+            <li> <a href="studentinfo.php">Student Information</a></li>
+            <li> <a href="schoolpersonnel.php">School Personnel</a></li>
+            <li> <a href="facultyload.php">Faculty Loads</a></li>
+        </ul>
     </aside>
 </body>
 </html>

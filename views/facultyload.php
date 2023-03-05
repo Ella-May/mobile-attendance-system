cb648ab6943c9cce6076fb9a8de7239d84731ec4
Ella-May <20191267@cityofmalabonuniversity.edu.ph>
2023-03-06 03:38:17 +0800

    display school personnel info and add data

--- views/facultyload.php
+++ views/facultyload.php
@@ -114,7 +114,6 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <!-- <script src="../public/assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../public/assets/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+    
 </body>
 </html>
